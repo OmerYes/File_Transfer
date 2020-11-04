c19032236329d02748ac774ba0e1ce0249f42db3
OmerYes <48590724+OmerYes@users.noreply.github.com>
2020-11-04 16:14:29 +0300

    --dosya seçme alanı css ve javascript düzenlendi

---
+++ File_Transfer/wwwroot/js/js_custom.js
@@ -0,0 +1,102 @@
+﻿
+//arka plan kayan resim
+addEventListener("load", function () {
+    setTimeout(hideURLbar, 0);
+}, false);
+
+function hideURLbar() {
+    window.scrollTo(0, 1);
+}
+
+
+//dosya yükleme
+
+$.fn.fileUploader = function (filesToUpload, sectionIdentifier) {
+    var fileIdCounter = 0;
+
+    this.closest(".files").change(function (evt) {
+        var output = [];
+
+        for (var i = 0; i < evt.target.files.length; i++) {
+            fileIdCounter++;
+            var file = evt.target.files[i];
+            var fileId = sectionIdentifier + fileIdCounter;
+
+            filesToUpload.push({
+                id: fileId,
+                file: file
+            });
+
+            //var removeLink = "<a class=\"removeFile\" href=\"#\" data-fileid=\"" + fileId + "\">Remove</a>";
+            var removeLink = "<i class=\"removeFile fa fa-trash\"  data-fileid=\"" + fileId + "\"></i>";
+            output.push("<li><strong>", file.name, "</strong> - ", file.size, " bytes. &nbsp; &nbsp; ", removeLink, "</li> ");
+        };
+
+        $(this).children(".fileList")
+            .append(output.join(""));
+
+        //reset the input to null - nice little chrome bug!
+        evt.target.value = null;
+    });
+
+    $(this).on("click", ".removeFile", function (e) {
+        e.preventDefault();
+
+        var fileId = $(this).parent().children("a").data("fileid");
+
+        // loop through the files array and check if the name of that file matches FileName
+        // and get the index of the match
+        for (var i = 0; i < filesToUpload.length; ++i) {
+            if (filesToUpload[i].id === fileId)
+                filesToUpload.splice(i, 1);
+        }
+
+        $(this).parent().remove();
+    });
+
+    this.clear = function () {
+        for (var i = 0; i < filesToUpload.length; ++i) {
+            if (filesToUpload[i].id.indexOf(sectionIdentifier) >= 0)
+                filesToUpload.splice(i, 1);
+        }
+
+        $(this).children(".fileList").empty();
+    }
+
+    return this;
+};
+
+(function () {
+    var filesToUpload = [];
+
+    var files1Uploader = $("#files1").fileUploader(filesToUpload, "files1");
+
+
+    $("#uploadBtn").click(function (e) {
+        e.preventDefault();
+
+        var formData = new FormData();
+
+        for (var i = 0, len = filesToUpload.length; i < len; i++) {
+            formData.append("files", filesToUpload[i].file);
+        }
+        formData.append("test", $("#test").val());
+        $.ajax({
+            url: "https://localhost:44382/Home/test",
+            data: formData,
+            processData: false,
+            contentType: false,
+            type: "POST",
+            success: function (data) {
+                alert("DONE");
+
+                files1Uploader.clear();
+                files2Uploader.clear();
+                files3Uploader.clear();
+            },
+            error: function (data) {
+                alert("ERROR - " + data.responseText);
+            }
+        });
+    });
+})()
